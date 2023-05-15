e6229db480d620a2a0bc4122f09db1e18388209a
Arshad_Qureshi <130859896+Arshad-Qureshi-Coder@users.noreply.github.com>
2023-05-15 23:22:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function threeSum(arr, target) {
 // write your code here
-  
+  let a=arr[0];
+	let b =arr[1];
+	let sum1 = arr[0]+arr[1];
+	for(let i=2;i<arr.length;i++){
+		sum1+=arr[i];
+		if(sum1>= target){
+			return sum1;
+		}
+	}
 }
 
 module.exports = threeSum;
